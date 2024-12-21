0cc42f715ee6a0823942838525561ac6e2853ddc
kaizansultan <kaizansultan@proton.me>
2024-12-21 23:56:31 +0700

    feat: write basic landing page layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+
+  vite: {
+    build: {
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler"
+        }
+      }
+    }
+  }
+})
